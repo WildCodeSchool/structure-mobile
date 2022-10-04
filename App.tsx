e5fa2b9d4020bd45bbc88a13c36dd2f356cb1505
marionptnt <marionpatinet@hotmail.fr>
2022-10-04 11:51:57 +0200

    Clean conficts

--- App.tsx
+++ App.tsx
@@ -3,39 +3,8 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-<<<<<<< HEAD
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { setContext } from "@apollo/client/link/context";
-import { client } from "./apollo/config";
-
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = AsyncStorage.getItem("token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token,
-    },
-  };
-});
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
-});
-// import { SecureStore } from "expo";
-import { ApolloProvider } from "@apollo/client";
-=======
 import { ApolloProvider } from "@apollo/react-hooks";
 import { client } from "./apollo/config";
->>>>>>> 2f2eba4 (Add react navigation, apollo client and graphql)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
