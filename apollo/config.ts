f35783040294fc991814d113efc0d0e80e41947b
Hochart-dev <hochart.dev@gmail.com>
2022-10-05 09:47:42 +0200

    fix login component , and set config.ts

--- apollo/config.ts
+++ apollo/config.ts
@@ -1,14 +1,21 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
+
+
+const { manifest } = Constants;
 
 const httpLink = createHttpLink({
-  uri: process.env.API_URI,
+  uri: manifest?.debuggerHost ? `http://${manifest.debuggerHost.split(':').shift()}:4000` : process.env.API_URI,
   credentials: "same-origin",
 });
 
+
+
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
+  
   const token = AsyncStorage.getItem("token");
   // return the headers to the context so httpLink can read them
   return {
