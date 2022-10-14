4b86d767c8aaa33ba2c4c24fb9a688292844d676
Nico786 <nicolas.theye@gmail.com>
2022-10-14 19:33:57 +0200

    add query to fetch projects: DOES NOT WORK YET

--- components/Project/Projects.tsx
+++ components/Project/Projects.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-
+import { GET_PROJECTS } from "../../apollo/queries";
+import { useQuery } from "@apollo/client";
 
 export default function Projects() {
+  const { data, loading, error } = useQuery(GET_PROJECTS);
+  if (loading)
+    return (
+      <View>
+        <Text>Chargement...</Text>
+      </View>
+    );
+  if (error)
+    return (
+      <View>
+        <Text>Oups, une erreur est survenue...</Text>
+      </View>
+    );
+  
+    console.log(data) // not working
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Liste des projets</Text>
